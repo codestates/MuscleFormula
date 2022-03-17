e009d4d8589621f5720306f6879e4f6de5bc95e0
jiyuunyang <jiyuunyang@gmail.com>
2022-03-17 18:15:13 +0900

    Feat: [Client] Nav컴포넌트 생성, Router 분기

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,13 @@ import App from './App';
 import store from './store/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
     <Provider store={store}>
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
     </Provider>,
   document.getElementById('root')
 );
