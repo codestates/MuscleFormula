9842c7eff9cb17c924c4c03e36d16b7c942b2d66
jiyuunyang <jiyuunyang@gmail.com>
2022-03-18 11:41:06 +0900

    Feat: [Client] mediaQuery로 반응형 구현

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,22 +1,40 @@
 /**네비게이션바**/
 import React from 'react';
 import {useSelector} from 'react-redux';
+import { Mobile, PC } from '../mediaQuery';
 import '../css/Nav.css';
 
 export default function Nav() {
   const state = useSelector(state => state.userInfoReducer);
   return (
     <header>
-      <div id='nav-container'>
+
+      <PC>
+      <div id='nav__container--PC'>
+        <span className='nav__title--PC'>
         <img id='logo' src='../logo.png' alt='logo'/>
-        <span className='nav-title'>근의 공식</span>
-        <ul className='nav-menu'>
+        근의 공식
+        </span>
+        <ul className='nav__menu--PC'>
           <li>운동기록</li>
           <li>알림</li>
           <li>채팅</li>
         </ul>
-        <span className='nav-user'>로그인</span>
+        <span className='nav__user--PC'>로그인</span>
       </div>
+      </PC>
+
+      <Mobile>
+      <div id='nav__container--M'>
+        <img id='icon__menu--M' src={require('../images/menu.png')} alt='menu'/>
+        <span className='nav__title--M'>
+        <img id='logo' src='../logo.png' alt='logo'/>
+          근의 공식
+        </span>
+        <span className='nav__user--M'>로그인</span>
+      </div>
+      </Mobile>
+
     </header>
   );
 }
