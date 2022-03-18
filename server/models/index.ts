152eaea6d3e6319fcba7be7fa2abaf721eb8fe8f
KAPUIST <thsxornjs12@gmail.com>
2022-03-18 09:13:10 +0900

    Dev:데이터베이스 오류수정

--- server/models/index.ts
+++
@@ -1,9 +0,0 @@
-import { createConnection } from "typeorm";
-
-createConnection()
-  .then(async () => {
-    console.log("mysql connected!!");
-  })
-  .catch((err) => console.log("connect err"));
-
-export default createConnection;
