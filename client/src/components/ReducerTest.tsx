0d7858753eaac5e083af23973fd732d9b8683591
jiyuunyang <jiyuunyang@gmail.com>
2022-03-20 21:33:37 +0900

    Feat: [Client] Redux-toolkit 적용

--- client/src/components/ReducerTest.tsx
+++ client/src/components/ReducerTest.tsx
@@ -1,28 +1,48 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { PLUS, MINUS, CHOICE} from '../reducer/counterReducer'
+import { ADD, QUAN, OUT } from '../reducer/testReducer';
+import { LOG_IN, LOG_OUT } from '../reducer/userInfoReducer';
+import {useState} from 'react';
+import type {RootState, AppDispatch} from '../store'
 
 function ReducerTest() {
 
   //redux안에 있는 모든 state들이 state입니다
   //let allstate = useSelector((state)=> state);
-  //
-  let test = useSelector((state:any) => state.testReducer.test);
-  let userInfo = useSelector((state:any) => state.userInfoReducer.userInfo); 
+  //store에서 state 설정해야함
+  const test = useSelector((state:RootState) => state.test.test); 
+  const count = useSelector((state:RootState) => state.counter.count); 
+  const user = useSelector((state:RootState) => state.userInfo.userInfo);
 
-  let dispatch = useDispatch();
+  let dispatch :AppDispatch = useDispatch();
   console.log('테스트용',test);
-  console.log('유저정보',userInfo);
+  console.log('카운터',count);
+  console.log('유저정보', user);
+
+  const [number, setNumber] = useState('');
+
+  const handleInputValue = (e :React.ChangeEvent<HTMLInputElement>) => {
+    setNumber(e.target.value);
+  }
 
   return (
     <div>
       <p>
-        <button onClick={()=> dispatch({type: 'ADD', payload: {id:2, name:'새상품', quan: 3}})}>항목추가</button>
-        <button onClick={()=> dispatch({type: 'OUT'})}>항목빼기</button>
-        <button onClick={()=> dispatch({type: 'QUANTITY_UP'})}>수량증가</button>
+        <button onClick={()=> dispatch(ADD({id:2, name:'새상품', quan: 3}))}>항목추가</button>
+        <button onClick={()=> dispatch(QUAN())}>수량증가</button>
+        <button onClick={()=> dispatch(OUT())}>항목빼기</button>
       </p>
       <p>
-        <button onClick={()=> dispatch({type: 'LOG_IN', payload: {id:1, nickname:'merong', img: './logo192.png'}})}>로그인</button>
-        <button onClick={()=> dispatch({type: 'LOG_OUT'})}>로그아웃</button>
+        <button onClick={()=> dispatch(LOG_IN({id:1, nickname:'koko', email: 'koko@naver.com'}))}>로그인</button>
+        <button onClick={()=> dispatch(LOG_OUT())}>로그아웃</button>
+      </p>
+      <p>
+        <button onClick={()=> dispatch(PLUS())}>PLUS</button>
+        <button onClick={()=> dispatch(MINUS())}>MINUS</button>
+        <input type="text" placeholder="값을 입력해주세요" onChange={handleInputValue}/>
+        {number}
+        <button onClick={()=> dispatch(CHOICE(Number(number)))}>CHOICE</button>
       </p>
     </div>
   );
