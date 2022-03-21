f8d3bc0d8eb9f638acaf67735a6680bee82e52f3
jiyuunyang <jiyuunyang@gmail.com>
2022-03-21 15:32:15 +0900

    Feature: [Client] Search 컴포넌트 목업

--- client/src/components/PostThumbnail.tsx
+++ client/src/components/PostThumbnail.tsx
@@ -1,8 +1,20 @@
 /**메인 화면에 뜨는 포스트 썸네일**/
+import styled from "styled-components"
+export const Postthumb = styled.div`
+
+`
+
 export default function PostThumbnail() {
   return (
-    <div>
-      
-    </div>
+    <Postthumb>
+      타이틀이미지
+      타이틀
+      좋아요수
+      프로필 사진
+      닉네임
+      소요시간
+      난이도
+      운동부위
+    </Postthumb>
   )
 }
--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,7 +1,16 @@
+import '../css/Main.css';
+import Search from '../components/Search';
+import PostThumbnail from '../components/PostThumbnail';
 export default function Main() {
-  console.log('이곳은 메인');
+  console.log('메인페이지');
   return (
-    <div>
+    <div id='main-container'>
+      <div className='search-container'>
+        <Search/>
+      </div>
+      <div className='postthumb-container'>
+        <PostThumbnail/>
+      </div>
     </div>
   )
 }
