6830736d8fd489fdf3d183fa072a2034749d29d1
olt13mm <olt13mm@gmail.com>
2022-03-21 22:14:10 +0900

    Dev: 저장

--- server/controllers/post/makePost.ts
+++ server/controllers/post/makePost.ts
@@ -4,4 +4,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-module.exports = async (req: Request, res: Response) => {};
+module.exports = async (req: Request, res: Response) => {
+  const { userId, postTitle, postImage, info, exerciseInfo, totalTime, difficult, bodyPart } = req.body;
+  console.log("makePost body : ", req.body);
+
+  
+
+
+};
