c6cbdb1f054c095bea098777909aa372d3237a9a
olt13mm <olt13mm@gmail.com>
2022-03-27 22:47:19 +0900

    Dev: save

--- server/routers/login.ts
+++ server/routers/login.ts
@@ -5,13 +5,18 @@ const google = require("../controllers/login/googleOauth");
 const kakao = require("../controllers/login/kakaoOauth");
 const signup = require("../controllers/user/signup");
 const readNick = require("../controllers/user/readNickname");
+
+const test = require("../controllers/login/testLongin");
+
 const loginrouter = express.Router();
 
 loginrouter.post("/up", signup);
 loginrouter.post("/in", emaillogin);
 loginrouter.post("/out", logout);
-loginrouter.get("/:nickname", readNick);
 loginrouter.get("/googleoauth", google);
 loginrouter.get("/kakaooauth", kakao);
 
+loginrouter.get("/test", test);
+
+// loginrouter.get("/:nickname", readNick);
 export default loginrouter;
