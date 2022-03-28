16954bb799c010a230c9b06cbbde9f50b73ebfd9
jiyuunyang <jiyuunyang@gmail.com>
2022-03-28 16:53:12 +0900

    Design [Client] CSS 수정

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React from "react";
+import { Mobile, PC } from "../mediaQuery";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { LOG_IN } from "../reducer/userInfoReducer";
@@ -7,17 +8,113 @@ import { useState, useEffect } from "react";
 import type { RootState, AppDispatch } from "../store";
 import styled from "styled-components";
 
-export const LoginBox = styled.div`
+export const LoginPC = styled.div`
+  margin-top: 1.5rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  min-height: 90vh;
+  > #login-container-left{
+      padding: 0rem 2rem;
+      display: flex;
+      flex-direction: column;
+    > #greeting-container {
+      display: flex;
+      flex-direction: row;
+      align-items: flex-end;
+      justify-content: space-between;
+      padding-bottom: 1.8rem;
+      > .greeting {
+        max-width: 150px;
+        font-size: x-large;
+        font-family: "IBM Plex Sans KR", sans-serif;
+      }
+      > img {
+        width: 50px;
+        height: 50px;
+      }
+    }
+    > #oauth-container {
+      > tr {
+        > th {
+          cursor: pointer;
+        }
+        > th, td {
+          font-size: small;
+          text-align: center;
+          color: grey;
+        }
+      }
+    }
+  }
+  > #login-container-right {
+    display: flex;
+    flex-direction: column;
+    > #user-container {
+      padding: 0rem 2rem;
+      > .user-input-container {
+        display: flex;
+        flex-direction: row;
+        margin-bottom: 1.2rem;
+        > div {
+          width: 4.5rem;
+          padding-right: 0.5rem;
+        }
+        > input {
+          border: none;
+          border-bottom: 2px solid black;
+          font-size: medium;
+        }
+        > input:focus {
+          outline: none;
+        }
+      }
+      > .button-container {
+        margin: 2rem 0rem 0.5rem 0rem;
+        display: flex;
+        justify-content: center;
+        > button {
+          cursor: pointer;
+          width: 100%;
+          height: 2rem;
+          border-radius: 10px;
+          border: none;
+          font-size: medium;
+          font-weight: bold;
+          background-color: #00cc99;
+          cursor: pointer;
+        }
+      }
+      > .signup-container {
+        font-size: small;
+        > .signup-link {
+          text-decoration: none;
+          color: #00cc99;
+          &:focus,
+          &:hover,
+          &:visited,
+          &link,
+          &:active {
+            text-decoration: none;
+            color: #00cc99;
+          }
+        }
+      }
+    }
+  }
+`
+
+export const LoginMobile = styled.div`
   /* 화면 중앙으로 만들기 */
   /* border: 3px solid red; */
   margin-top: 1.5rem;
   display: flex;
+  flex: column;
   justify-content: center;
   align-items: center;
   min-height: 90vh;
   > #login-container {
-    border: 3px solid red;
-
     display: flex;
     flex-direction: column;
     > #greeting-container {
@@ -91,13 +188,9 @@ export const LoginBox = styled.div`
       padding: 1rem 2rem;
       > tr {
         > th {
-          > img {
-            width: 30px;
-            cursor: pointer;
-          }
+          cursor: pointer;
         }
-        > td,
-        th {
+        > th, td {
           font-size: small;
           text-align: center;
           color: grey;
@@ -178,8 +271,75 @@ export default function LoginTest() {
   };
 
   return (
-    <LoginBox>
-      <div id="login-container">
+    <div>
+    <PC>
+    <LoginPC>
+      <div id="login-container-left">
+        <div id="greeting-container">
+          <div className="greeting">오늘도 힘차게 운동해볼까요?</div>
+          <img src="../images/icon_exerciseman.png" alt="exercising_man" />
+        </div>
+        <table id="oauth-container">
+          <tr>
+            <th>
+              <img
+                src="../images/icon_google.png"
+                alt="logoGoogle"
+                onClick={loginKakaoHangle}
+              />
+            </th>
+            <th>
+              <a href={kakaoCodeGetURI} onClick={getCodeClickHandler}>
+                <img
+                  src="../images/icon_kakao.png"
+                  alt="logoKakao"
+                  onClick={() => {
+                    console.log("kakao");
+                  }}
+                />
+              </a>
+            </th>
+          </tr>
+          <tr>
+            <td>구글로 로그인</td>
+            <td>카카오로 로그인</td>
+          </tr>
+        </table>
+      </div>
+      <div id="login-container-right">
+        <div id="user-container">
+          <div className="user-input-container">
+            <div>아이디</div>
+            <input
+              type="text"
+              placeholder="아이디를 입력해주세요"
+              onChange={changeEmail}
+            />
+          </div>
+          <div className="user-input-container">
+            <div>비밀번호</div>
+            <input
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              onChange={changePassword}
+            />
+          </div>
+          <div className="button-container">
+            <button onClick={loginHangle}>로그인</button>
+          </div>
+          <div className="signup-container">
+            아이디가 없으시나요?{" "}
+            <Link className="signup-link" to="/signup">
+              회원가입
+            </Link>
+          </div>
+        </div>
+      </div>
+    </LoginPC>
+    </PC>
+    <Mobile>
+    <LoginMobile>
+    <div id="login-container">
         <div id="greeting-container">
           <div className="greeting">오늘도 힘차게 운동해볼까요?</div>
           <img src="../images/icon_exerciseman.png" alt="exercising_man" />
@@ -238,6 +398,8 @@ export default function LoginTest() {
           </tr>
         </table>
       </div>
-    </LoginBox>
+    </LoginMobile>
+    </Mobile>
+    </div>
   );
 }
