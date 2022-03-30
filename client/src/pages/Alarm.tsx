1256188ba32915f15f3e43cf203567fe0791c95a
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2022-03-30 22:25:47 +0900

    Delete client/src/pages directory

--- client/src/pages/Alarm.tsx
+++
@@ -1,9 +0,0 @@
-/**메신저 채팅 페이지**/
-export default function Alarm() {
-  console.log('알림 페이지')
-  return (
-    <div>
-      
-    </div>
-  )
-}
--- client/src/pages/Editor.tsx
+++
@@ -1,8 +0,0 @@
-/**포스트 작성 에디터**/
-export default function Editor() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
