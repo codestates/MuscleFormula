0903490fc5544402bd1cadaaf8b6a9095a68600f
jiyuunyang <jiyuunyang@gmail.com>
2022-03-30 11:33:57 +0900

    Feature: [Client] Sever에서 레코드 가져오기

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,13 +1,7 @@
 export default function Landing() {
-<<<<<<< HEAD
-  console.log("랜딩페이지");
-  return <div></div>;
-}
-=======
   console.log('랜딩페이지')
   return (
     <div>
     </div>
   )
 }
->>>>>>> 9ea44ea958d99d6dcb278f62416aa79ba8eec0ae
