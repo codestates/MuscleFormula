e7e11388107e6251b75a165329bd5f3ab3b61a7f
KAPUIST <thsxornjs12@gmail.com>
2022-03-30 12:01:40 +0900

    Fix:응답수정

--- server/controllers/user_record/createUserExRecord.ts
+++ server/controllers/user_record/createUserExRecord.ts
@@ -76,7 +76,6 @@ module.exports = async (req: Request, res: Response) => {
           where: { record: a },
         });
         res.status(200).json({
-          RecordId: a,
           Records: returndata,
         });
       }, 1000);
