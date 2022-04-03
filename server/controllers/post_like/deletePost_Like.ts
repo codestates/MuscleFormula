589e9cd8f8fb69f18c5d31e22fdee75e4b2b83c0
olt13mm <olt13mm@gmail.com>
2022-04-03 19:59:17 +0900

    Feat: post like기능 완성(수정필요)

--- server/controllers/post_like/deletePost_Like.ts
+++ server/controllers/post_like/deletePost_Like.ts
@@ -9,9 +9,9 @@ dotenv.config();
 module.exports = async (req: Request, res: Response) => {
   console.log("server deletePost_Like in !!");
 
-  const likeId = req.params.id;
+  const postId = req.params.id;
   const auth = req.headers["authorization"];
-  console.log("likeId :", likeId);
+  console.log("postId :", postId);
   console.log("auth :", auth);
   if (!auth) {
     res.status(401).send({ messege: "엑세스 토큰이 존재하지 않습니다." });
@@ -28,25 +28,29 @@ module.exports = async (req: Request, res: Response) => {
         const user = await getRepository(Users).findOne({
           where: { id: data.id },
         });
+        const post = await getRepository(Posts).findOne({
+          where: { id: postId },
+        });
 
         const postLike: any = await getRepository(Post_Likes).findOne({
-          where: { id: likeId },
+          where: { users: data.id, post: postId },
           relations: ["users"],
         });
         console.log("postLike:", postLike);
-        //console.log("user:", user);
-        //console.log("post:", post);
+        console.log("user:", user);
+        console.log("post:", post);
+        console.log("data:", data);
 
-        if (postLike.users.email === data.email) {
-          try {
-            await postLike.remove();
-            res.status(200).json({ message: `like 삭제 성공` });
-          } catch (e) {
-            res.status(400).send({ message: "like 삭제 실패" });
-          }
-        } else {
-          res.status(404).send({ message: "like가 존재하지 않음 (id확인)" });
+        // if (postLike.users.email === data.email) {
+        try {
+          await postLike.remove();
+          res.status(200).json({ message: `like 삭제 성공` });
+        } catch (e) {
+          res.status(400).send({ message: "like 삭제 실패" });
         }
+        // } else {
+        //   res.status(404).send({ message: "like가 존재하지 않음 (id확인)" });
+        // }
       }
     });
     // console.log(data.email);
