663b96fabe020af44ad49125b421090bec489532
KAPUIST <thsxornjs12@gmail.com>
2022-04-04 17:15:36 +0900

    중간저장

--- server/server.ts
+++ server/server.ts
@@ -5,9 +5,9 @@ dotenv.config();
 const http = require("http");
 const PORT = process.env.PORT;
 
-const server = http.createServer(app);
+const server = require("./socket");
 
-console.log("port:",PORT)
+console.log("port:", PORT);
 server.listen(PORT, () => {
   console.log(`port:${PORT} server is on!`);
 });
