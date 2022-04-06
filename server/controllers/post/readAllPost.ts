76c760f3a56ea8d8ae54a95fc41ebd7d54660193
KAPUIST <thsxornjs12@gmail.com>
2022-04-06 17:42:56 +0900

    fix:data

--- server/controllers/post/readAllPost.ts
+++ server/controllers/post/readAllPost.ts
@@ -56,37 +56,60 @@ module.exports = async (req: Request, res: Response) => {
   } else if (rankData.length > 3) {
     for (let i = 0; i < 3; i++) {
       rankData[i].total_time = showtime(rankData[i].total_time);
+      const createed = allInfo.map((item) => {
+        const data = {
+          user: {
+            userId: item.users.id,
+            nickname: item.users.nickname,
+            image: item.users.image,
+          },
+          postId: item.id,
+          postTitle: item.title,
+          info: item.info,
+          postImage: item.image,
+          bodyPart: item.body_Part,
+          difficult: item.difficult,
+          totalTime: item.total_time,
+          total_comments: item.post_comments.length,
+          total_Likes: item.post_likes.length,
+          created_At: item.created_At,
+        };
+        return data;
+      });
+      res.status(200).json({
+        rankData: rankData,
+        posts: createed,
+      });
     }
   } else {
     for (let i = 0; i < rankData.length; i++) {
       rankData[i].total_time = showtime(rankData[i].total_time);
+      const createed = allInfo.map((item) => {
+        const data = {
+          user: {
+            userId: item.users.id,
+            nickname: item.users.nickname,
+            image: item.users.image,
+          },
+          postId: item.id,
+          postTitle: item.title,
+          info: item.info,
+          postImage: item.image,
+          bodyPart: item.body_Part,
+          difficult: item.difficult,
+          totalTime: item.total_time,
+          total_comments: item.post_comments.length,
+          total_Likes: item.post_likes.length,
+          created_At: item.created_At,
+        };
+        return data;
+      });
+      res.status(200).json({
+        rankData: rankData,
+        posts: createed,
+      });
     }
 
     console.log(rankData);
-
-    const createed = allInfo.map((item) => {
-      const data = {
-        user: {
-          userId: item.users.id,
-          nickname: item.users.nickname,
-          image: item.users.image,
-        },
-        postId: item.id,
-        postTitle: item.title,
-        info: item.info,
-        postImage: item.image,
-        bodyPart: item.body_Part,
-        difficult: item.difficult,
-        totalTime: item.total_time,
-        total_comments: item.post_comments.length,
-        total_Likes: item.post_likes.length,
-        created_At: item.created_At,
-      };
-      return data;
-    });
-    res.status(200).json({
-      rankData: rankData,
-      posts: createed,
-    });
   }
 };
