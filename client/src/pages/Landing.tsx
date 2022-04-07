22d709949c31cf683daf226e62db11c549eadeb4
jiyuunyang <jiyuunyang@gmail.com>
2022-04-07 16:48:43 +0900

    Design: [Client] 랜딩페이지 첫페이지 수정

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 interface Idx {
   idx: number;
 }
+
+
 
 export const AllLandingContainer = styled.div`
   display: flex;
@@ -11,7 +13,6 @@ export const AllLandingContainer = styled.div`
   width: 100%;
   justify-content: center;
   align-items: center;
-  font-family: "IBM Plex Sans KR", sans-serif;
   transition: 0.5s all;
   @media screen and (max-width: 1000px) {
     width: 100%;
@@ -25,7 +26,6 @@ export const BodyContainer = styled.main`
   width: 100%;
   max-width: 78.75rem;
   padding: 0 30px 0 30px;
-  background: linear-gradient(0deg, #fbfafc, #fbfafc);
   @media screen and (max-width: 1000px) {
     padding: 0;
   }
@@ -39,14 +39,13 @@ export const BodyOutContainer = styled.main`
 
 export const FirstLandingContainer = styled.div`
   width: 100%;
-  height: 51.313rem;
+  padding-top: 8rem;
+  height: 30rem;
   display: flex;
   align-items: center;
-  background: linear-gradient(0deg, #fbfafc, #fbfafc);
   transition: 0.5s all;
   position: relative;
   @media screen and (max-width: 1000px) {
-    margin: 70px 0 70px 0;
     height: auto;
     position: static;
     display: flex;
@@ -57,6 +56,12 @@ export const FirstLandingContainer = styled.div`
   }
 `;
 
+const leftToRight = keyframes`
+  0% {left:0; opacity: 0}
+  50% {left: 10rem; opacity: 1}
+  100% {left:10rem; opacity: 1}
+`
+
 export const FirstTextContainer = styled.div`
   display: flex;
   height: 100%;
@@ -65,6 +70,9 @@ export const FirstTextContainer = styled.div`
   transition: 0.5s all;
   z-index: 2;
   position: absolute;
+  animation-name: ${leftToRight};
+  animation-duration: 3s;
+  animation-iteration-count: infinite;
   @media screen and (max-width: 1000px) {
     position: static;
     margin-top: 20px;
@@ -73,13 +81,23 @@ export const FirstTextContainer = styled.div`
     align-items: center;
     text-align: center;
     width: 100%;
-    height: 313px;
+    height: 250px;
+  }
+  @media screen and (max-width: 37.5rem) {
+    margin-top: 0;
+    height: 200px;
   }
   @media screen and (max-width: 420px) {
     margin-top: 0;
-    height: 250px;
+    height: 150px;
   }
 `;
+
+const rightToLeft = keyframes`
+  0% {right:0; width:100px;}
+  50% {right:10rem; width:229px;}
+  100% {right:10rem;}
+`
 
 export const FirstImageContainer = styled.div`
   position: absolute;
@@ -89,88 +107,90 @@ export const FirstImageContainer = styled.div`
   transition: 0.5s all;
   z-index: 1;
   right: 0;
+  animation-name: ${rightToLeft};
+  animation-duration: 3s;
+  animation-iteration-count: infinite;
+  margin-bottom: 3rem;
+
   img {
-    width: 700px;
+    width: 100%;
   }
+
   @media screen and (max-width: 1000px) {
     position: static;
     width: 100%;
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
-    margin-bottom: 0px;
     img {
-      width: 500px;
+      width: 150px;
     }
   }
   @media screen and (max-width: 37.5rem) {
-    margin-bottom: 0;
     img {
-      width: 317px;
+      width: 120px;
     }
   }
   @media screen and (max-width: 420px) {
     img {
-      width: 287px;
+      width: 100px;
     }
   }
 `;
 
 export const FirstText = styled.div`
   font-weight: 100;
-  font-size: 1.5rem;
+  font-size: xx-large;
   transition: 0.5s all;
   span {
-    margin-bottom: 0.813rem;
+    margin-bottom: 1rem;
   }
   @media screen and (max-width: 1000px) {
     div {
-      margin-bottom: 4px;
+      margin-bottom: 0.2rem;
     }
   }
   @media screen and (max-width: 37.5rem) {
-    font-size: 18px;
+    font-size: x-large;
     transition: 0.5s all;
   }
   @media screen and (max-width: 420px) {
-    font-size: 14px;
+    font-size: large;
   }
 `;
 
 export const FirstSecondText = styled.div`
-  font-weight: 300;
-  font-size: 1.875rem;
-  margin: 0.5rem 0 2.75rem 0;
+  font-size: xx-large;
+  margin: 0.5rem 0 2rem 0;
   transition: 0.5s all;
+  font-family: "IBM Plex Sans KR", sans-serif;
   @media screen and (max-width: 1000px) {
-    margin: 39px 0 45px 0;
   }
   @media screen and (max-width: 37.5rem) {
-    font-size: 20px;
+    font-size: x-large;
     transition: 0.5s all;
-    margin: 39px 0 45px 0;
     text-shadow: none;
   }
   @media screen and (max-width: 420px) {
-    font-size: 16px;
-    margin: 20px 0 40px 0;
+    font-size: large;
   }
 `;
 
 export const GotoMainButton = styled.button`
   font-family: "IBM Plex Sans KR", sans-serif;;
-  font-weight: 100;
-  width: 13.188rem;
-  height: 4.063rem;
-  background: #2d2d2d;
-  border-radius: 4px;
+  width: 15rem;
+  height: 4rem;
+  background: black;
+  padding: 0.3rem;
+  border-radius: 10px;
   box-sizing: border-box;
   cursor: pointer;
-  font-size: 1.5rem;
+  font-size: x-large;
   color: #ffffff;
   border: none;
+  box-shadow: 2px 3px 10px 2px rgba(0, 0, 0, 0.2);
   transition: 0.5s all;
-  :hover {
+  &:hover {
     background: #3f3f3f;
   }
 `;
@@ -247,6 +267,14 @@ export const VerySmallCircle = styled.span`
     top: 468px;
   }
 `;
+
+export const FirstLandingButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem 0rem 5rem 0rem;
+`
 
 export const SecLandingContainer = styled.div`
   display: flex;
@@ -591,20 +619,6 @@ export const GoTopContainer = styled.div`
 function Landing() {
   const SecondLandingPageTxt = [
     {
-      title: "순위 경쟁",
-      img: "./images/icon_goldmedal.png",
-
-      descr: [
-        "근의 공식은 운동 기록을 통해",
-        "다른 유저와 경쟁할 수 있는 플랫폼입니다. ",
-      ],
-    },
-    {
-      title: "운동 게시물 확인",
-      img: "./images/icon_bulletin-board-9123.svg",
-      descr: ["근의 공식에서 최신 운동 관련 게시물을 ", "한눈에 확인 할 수 있습니다."],
-    },
-    {
       title: "실시간 기록 확인",
       img: "./images/icon_24-hours-timer-12573.svg",
       descr: [
@@ -612,20 +626,32 @@ function Landing() {
         "본인의 운동기록을 초단위로 기록할 수 있습니다.",
       ],
     },
+    {
+      title: "순위 경쟁",
+      img: "./images/icon_goldmedal.png",
+      descr: [
+        "근의 공식은 운동 기록을 통해",
+        "다른 유저와 경쟁할 수 있는 플랫폼입니다. ",
+      ]
+    },
+    {
+      title: "운동 게시물 확인",
+      img: "./images/icon_bulletin-board-9123.svg",
+      descr: ["근의 공식에서 최신 운동 관련 게시물을 ", "한눈에 확인 할 수 있습니다."],
+    },
   ];
 
-  const LandingPageTxt = [
+  const ThirdLandingPageTxt = [
     {
-      title: ["01", "운동 세부 기록을", "확인해보세요"],
+      title: ["01", "운동 세부 기록을 확인해보세요"],
       img: "./images/Landing_page1.svg",
       descr: [
-        "근의 공식은 자신의 운동 기록을 공유하며 다른 유저들과 소통할 수 있는 커뮤니티입니다",
-        "체계적인 운동 루틴",
-        "운동부위, 난이도, 총 소요시간을 선택해 보세요! ",
+        "체계적인 운동 루틴을 짜고",
+        "초 단위로 기록을 확인할 수 있습니다"
       ],
     },
     {
-      title: ["02", "유저들간", "순위 경쟁을 해보세요"],
+      title: ["02", "유저들간 순위 경쟁을 해보세요"],
       img: "./images/Landing_page2.png",
       descr: [
         "근의 공식은 상위 3위 랭킹의 유저들에게",
@@ -635,13 +661,14 @@ function Landing() {
     {
       title: [
         "03",
-        "게시물을 확인해",
-        "다른 유저들은 어떤 운동을 하는지 확인해보세요",
+        "다른 사람들은 어떤 운동을 하는지",
+        "게시물을 통해 확인해보세요",
       ],
       img: "./images/Lading_page3.svg",
       descr: [
-        "근의 공식은 ",
-        "디테일한 운동 습관을 형성해 줍니다.",
+        "근의 공식은 자신의 운동 기록을 공유하며", 
+        "다른 유저들과 소통할 수 있는 커뮤니티입니다",
+        "운동부위, 난이도, 소감을 공유해보세요",
       ],
     },
   ];
@@ -659,19 +686,22 @@ function Landing() {
               <FirstText>
                 <div>침대에 누워만 있는 당신</div>
                 <div>열정만 가지고 오세요</div>
-                <div>함께 운동하자 GO! </div>
+                <div>함께 운동하자 GO</div>
               </FirstText>
               <FirstSecondText>
-                근의공식에서 운동기록을 쌓고 교류해보세요!
+                <div>근의공식에서 운동기록을 쌓고</div>
+                <div>사람들과 교류해보세요</div>
               </FirstSecondText>
-              <Link to="/main">
-                <GotoMainButton> 근의 공식 시작하기</GotoMainButton>
-              </Link>
             </FirstTextContainer>
             <FirstImageContainer>
-              <img src="./images/Landing_page1.jpg" alt="landingpage_img1" />
+              <img src="./logo.png" alt="logo" />
             </FirstImageContainer>
           </FirstLandingContainer>
+          <FirstLandingButton>
+            <Link to="/main">
+              <GotoMainButton> 근의 공식 시작하기</GotoMainButton>
+            </Link>
+          </FirstLandingButton>
         </BodyContainer>
       </BodyOutContainer>
       <SecLandingContainer>
@@ -697,7 +727,7 @@ function Landing() {
       </SecLandingContainer>
       <ThirdBodyOutContainer>
         <ThirdBodyContainer>
-          {LandingPageTxt.map((el, idx) => {
+          {ThirdLandingPageTxt.map((el, idx) => {
             return (
               <ThirLandingContainer idx={idx} key={el.title[0]}>
                 <AllContainer idx={idx}>
